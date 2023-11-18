edbe1c83ea2719e125c243a51662a779cd377249
yuichiroyamaji <yuichiroyamaji@hotmail.com>
2023-11-18 21:13:24 +0900

    amplify add api / amplify push

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "narejiro": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
