7a950824f2d8d7cd77568c387c879e84acf35b20
yuichiroyamaji <yuichiroyamaji@hotmail.com>
2024-04-10 20:11:45 +0900

    reinstall amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "narejiro2": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
